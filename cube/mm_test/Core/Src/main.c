178a07503f0bcd733a7d3b5a1954aa668a4df437
anthoir2 <anthoir2@uci.edu>
2024-04-08 12:51:43 -0700

    this is a test for egit

--- cube/mm_test/Core/Src/main.c
+++ cube/mm_test/Core/Src/main.c
@@ -17,7 +17,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "main.h"  /*this is a comment*/
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
