debb2d0418a72fbd345eacde14f0661c7f06ffe4
anthoir2 <anthoir2@uci.edu>
2024-04-08 13:13:09 -0700

    this is a push test

--- cube/mm_test/Core/Src/main.c
+++ cube/mm_test/Core/Src/main.c
@@ -17,7 +17,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"  /*this is a new neweew3 comment*/
+#include "main.h"  /*this is a old comment*/
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
