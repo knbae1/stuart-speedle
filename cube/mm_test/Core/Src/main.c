c9d118b658b6f2fa7dfc98c7771f8c9af9289743
anthoir2 <anthoir2@uci.edu>
2024-04-08 13:13:36 -0700

    old

--- cube/mm_test/Core/Src/main.c
+++ cube/mm_test/Core/Src/main.c
@@ -17,7 +17,7 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"  /*this is a old comment*/
+#include "main.h"  /*this is a old old comment*/
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
