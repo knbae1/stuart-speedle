8597d192a4ac15ad8072ebfca1148dda619a672a
anthoir2 <anthoir2@uci.edu>
2024-05-18 16:07:43 -0700

    added code to define position in terms of encoder ticks

--- cube/mm_test2/Core/Src/main.c
+++ cube/mm_test2/Core/Src/main.c
@@ -69,6 +69,8 @@ uint16_t enc_left = 0;
 uint16_t enc_right = 0;
 int16_t enc_left_typeC;
 int16_t enc_right_typeC;
+int16_t position_left = 0;
+int16_t position_right = 0;
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 	// this is the left encoder timer
 	/*if (htim->Instance == TIM3) {
@@ -77,10 +79,12 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
 	if (htim->Instance == TIM3) {
 		enc_left = __HAL_TIM_GET_COUNTER(htim);
 		enc_left_typeC = (int16_t)enc_left;
+		position_left =  -1*enc_left_typeC/30;
 	}
 	if (htim->Instance == TIM4) {
 		enc_right = __HAL_TIM_GET_COUNTER(htim);
 		enc_right_typeC = (int16_t)enc_right;
+		 position_right = -1*enc_right_typeC/30;
 	}
 
 }
@@ -197,9 +201,9 @@ int main(void)
 
 	  //rotate_right(3000);
 
-	  move_fwd(6000);
+	  //move_fwd(6000);
 
-	  //move_bwd(2000);
+	  move_bwd(2000);
 
     /* USER CODE END WHILE */
 
